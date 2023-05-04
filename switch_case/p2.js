f43c16bee79329e808f11511f91a944b28d0e2bc
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-04 18:26:17 +0530

    newfile

---
+++ switch_case/p2.js
@@ -0,0 +1,26 @@
+let a,b,c,ch;
+document.write("1.Addition<br>");
+document.write("2.Subtraction<br>");
+document.write("3.multiplication<br>");
+document.write("4.division<br>");
+document.write("Enter your choice:-<br>");
+document.write("\nEnter a & b:-<br>");
+switch (ch)
+{
+case 1: c=a+b;
+document.write("Addition is"+c);
+    break;
+case 2: c=a-b;
+document.write("Subtraction is"+c);
+    break;
+case 3: c=a*b;
+document.write("Multiplication is"+c);
+    break; 
+case 4: c=a/b;
+document.write("division is"+c);
+    break;     
+
+default:
+    document.write("\nInvalid choice");
+    break;
+}
---
+++ switch_case/p3.js
@@ -0,0 +1,28 @@
+let userInput = prompt("Enter a letter for a day of the week: ");
+
+switch (userInput) {
+  case 'm':
+    document.write("Monday");
+    break;
+  case 't':
+    document.write("Tuesday");
+    break;
+  case 'w':
+    document.write("Wednesday");
+    break;
+  case 'th':
+    document.write("Thursday");
+    break;
+  case 'f':
+    document.write("Friday");
+    break;
+  case 'sa':
+    document.write("Saturday");
+    break;
+  case 'su':
+    document.write("Sunday");
+    break; 
+  default:
+    document.write("Invalid input. Please enter a letter for a day of the week.");
+    break;
+}
