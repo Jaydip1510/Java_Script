8c0a13bc3f8c731d4c77bca4a9847140f1c0dc12
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-04 17:58:50 +0530

    new file

---
+++ switch_case/p1.js
@@ -0,0 +1,13 @@
+let ch='c';
+let a=10,b=20;
+switch(ch){
+          case 'c':c=a+b;
+                 document.write("Addition is:-"+c+"<br>");
+                 break;
+          case 'c':c=a*b;
+                 document.write("Multiplication is:-"+c);
+                 break;
+          default:
+                document.write("Invalid choice");
+                break;
+}
