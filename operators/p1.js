982e26f5623f53485ae5dc185c16af0b3d328cff
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-04 15:54:48 +0530

    new file

---
+++ operators/p1.js
@@ -0,0 +1,16 @@
+let x=5,y;
+// x**=3;
+x=x**2;
+document.getElementById('demo').innerHTML="square is:-"+x;
+
+let a="20";
+let b=20;
+if(a===b){
+        document.write("A & B is same");
+}else{
+       document.write("A & B is not same<br>");
+}
+document.write("Type of var a:- "+typeof(a)+"<br>");
+document.write("Type of var b:- "+typeof(b)+"</br>");
+
+      
