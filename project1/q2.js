26933364d74699587521815b56bf9356a8bbd982
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-04 16:25:22 +0530

    new

---
+++ project1/q2.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 7;
+
+document.write("Before swapping a:-"+a);
+document.write("Before swapping b:-"+b);
+let temp = a;
+a = b;
+b = temp;
+document.write("After swapping a:- "+a); 
+document.write("After swapping b:- "+b); 
