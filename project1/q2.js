195391768694e991ecd7b2504a6c4a0392afd94a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-04 17:13:18 +0530

    update

--- project1/q2.js
+++ project1/q2.js
@@ -1,10 +1,10 @@
 let a = 5;
 let b = 7;
 
-document.write("Before swapping a:-"+a);
-document.write("Before swapping b:-"+b);
+document.write("Before swapping a:-"+a+"<br>");
+document.write("Before swapping b:-"+b+"<br>");
 let c = a;
 a = b;
 b = c;
-document.write("After swapping a:- "+a); 
-document.write("After swapping b:- "+b); 
+document.write("After swapping a:- "+a+"<br>"); 
+document.write("After swapping b:- "+b+"<br>"); 
