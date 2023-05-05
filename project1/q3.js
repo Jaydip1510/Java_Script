a99cf4a1228626881731f76ede5869662908c568
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-05 10:44:29 +0530

    update

--- project1/q3.js
+++ project1/q3.js
@@ -1,4 +1,4 @@
 let celsius = 25;
 let fahrenheit = (celsius * 9/5) + 32;
 
-document.write(+(celsius)+"°C is equal to" +(fahrenheit)+"°F");
+document.write(+(celsius)+"C is equal to" +(fahrenheit)+"F");
