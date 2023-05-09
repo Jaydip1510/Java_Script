2e5631de8f94433bb1c8819d7940308ec07266f0
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-09 15:23:44 +0530

    file

--- practical_exam/q1.js
+++ practical_exam/q1.js
@@ -1,1 +1,15 @@
-let unit = prompt("Enter the Unit:-");
+let units = 700; 
+
+let price;
+
+if(units >= 0 && units <= 100) {
+  price = units * 4.90;
+} else if(units > 100 && units <= 500) {
+  price = 100 * 4.90 + (units - 100) * 5.50;
+} else if(units > 500 && units <= 1000) {
+  price = 100 * 4.90 + 400 * 5.50 + (units - 500) * 6.15;
+} else {
+  document.write("Invalid input. Units should be between 0 and 1000.");
+}
+
+document.write("Price is" + price.toFixed(2));
