1c9181df08d62eb80a1ab34dfb99c616d38b7949
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-10 16:52:54 +0530

    update

--- work/check.js
+++ work/check.js
@@ -2,12 +2,15 @@ let a = prompt("Enter Value of A:-")
 let b = prompt("Enter Value of B:-");
 let answer;
 
-if (a == 5 && b == 10) {
-  answer = 100;
-} else if (a !== 5) {
-  answer = 0;
-} else {
-  answer = 50;
+if(a == 5){
+      if(b==10){
+        answer=100;
+      }else{
+        answer=50;
+      }
+}else{
+    answer=0;
 }
+
 
 document.write("Answer is:-"+answer);
