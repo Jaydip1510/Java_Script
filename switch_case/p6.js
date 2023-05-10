ae0a82f583acab7aef868c1199f6fa4acf4ce396
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-10 17:45:58 +0530

    new addfile

---
+++ switch_case/p6.js
@@ -0,0 +1,14 @@
+let ch = prompt("Enter Any Choice:-");
+switch(ch){
+       case 'a':
+       case 'e':
+       case 'i':
+       case 'o':
+       case 'u':
+            document.write(ch+"&nbspis vowel");
+            break;
+      default:
+           document.write(ch+"&nbspis Consonant");
+           break;  
+
+}
