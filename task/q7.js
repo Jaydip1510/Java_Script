d4fb38c56df09ec5f54b8c5be52553ce148c0d09
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-15 10:42:53 +0530

    newfile

---
+++ task/q7.js
@@ -0,0 +1,14 @@
+let no = prompt("Enter Any number:-");
+
+if(no%2==0){
+      document.write(no+"&nbspNumber is even<br>");
+}else{
+      document.write(no+"&nbspNumber is odd<br>");
+}
+let year = prompt("Enter Any year:-");
+
+if(year%4==0){
+      document.write(year+"&nbspis leap year");
+}else{
+     document.write(year+"&nbspis not leap year");
+}
