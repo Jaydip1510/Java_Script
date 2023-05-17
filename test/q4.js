f339070d06b06c95ba2aa74e2a1346b9ff02e51e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-17 10:45:53 +0530

    update

--- test/q4.js
+++ test/q4.js
@@ -1,11 +1,13 @@
-const character = prompt("Enter a character:");
+const ch = prompt("Enter a character:-");
 
-if (character.match(/[a-z]/)) {
-  if (['a', 'e', 'i', 'o', 'u'].includes(character)) {
-    document.write("The character is a vowel.");
-  } else {
-    document.write("The character is a consonant.");
-  }
-} else {
-  document.write("Invalid character.");
+if(ch >= 'a' && ch<='z' || ch >='A' && ch <= 'Z' ){
+    if(ch == 'a' || ch == 'e'  || ch == 'i' || ch =='o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+      document.write(ch+"&nbspis Vowel" + "<br>");
+    }
+    else{
+      document.write(ch+"&nbspis Consonant" + "<br>");
+    }
+}else{
+     document.write("Invalid Character");
 }
+
