adcc782a302bdf7dcf5b22c73100ff640fb86652
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-17 12:35:36 +0530

    add

---
+++ for_each/p1.js
@@ -0,0 +1,10 @@
+let a = [25,65,78,14,98,77];
+
+// a.forEach(display);
+// function display(val,pos){
+//     document.write("Element is:"+pos+"---"+val+"<br>")
+// }
+
+a.forEach(function(val,i){
+     document.write("Element is:"+i+"----"+val+"<br>");
+});
