56a13c88914faed0f69fdac419ebe533eb1f4687
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-17 16:54:32 +0530

    add

--- for_each/p1.js
+++ for_each/p1.js
@@ -1,10 +1,10 @@
 let a = [25,65,78,14,98,77];
 
-// a.forEach(display);
-// function display(val,pos){
-//     document.write("Element is:"+pos+"---"+val+"<br>")
+a.forEach(function(a){document.write("Element is:---"+a+"<br>");});
+// function display(a,b){
+// //     document.write("Element is:"+a+"---"+b+"<br>");
 // }
 
-a.forEach(function(val,i){
-     document.write("Element is:"+i+"----"+val+"<br>");
-});
+// a.forEach(function(val,i){
+//      document.write("Element is:"+i+"----"+val+"<br>");
+// });
