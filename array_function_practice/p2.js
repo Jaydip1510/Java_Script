9d308e0733cbe38e9046961650b13fd4649c234d
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-19 17:35:47 +0530

    added

---
+++ array_function_practice/p2.js
@@ -0,0 +1,55 @@
+let arr = ["abc","pqr","xyz","def","jkl"];
+let i;
+
+// let arr2 = arr.toString();
+// document.write("String is:-"+arr2+"<br>");
+
+
+// arr.pop();
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+// }
+
+// arr.push("jay");
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+// }
+
+// document.write("String length is:-"+arr.length+"<br>");
+
+
+// arr.shift();
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+// }
+
+// arr.unshift("jay");
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+// }
+
+
+// arr[2] = "raj";
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+// }
+
+
+
+// delete arr[2];
+//    for(i=0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>");
+//    }
+
+
+// let a = ["jay","raj","dhoni"];
+// let b = ["abc","pqr","xyz"];
+// let c = arr.concat(a,b);
+// for(i=0;i<c.length;i++){
+//    document.write(c[i]+"<br>");
+// }
+
+arr.splice(1,2,"jaydip","makwana");
+for(i=0;i<arr.length;i++){
+   document.write(arr[i]+"<br>");
+}
