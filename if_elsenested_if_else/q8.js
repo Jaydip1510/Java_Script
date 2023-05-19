36113758fc3c7ee9d9d477f6c0f87ad1c46040b4
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-19 07:26:28 +0530

    changes

--- if_elsenested_if_else/q8.js
+++ if_elsenested_if_else/q8.js
@@ -1,17 +1,17 @@
 function incrementSalary(salary) {
     if (salary >= 1000 && salary <= 5000) {
-      return salary + 1000;
+      return parseInt(salary) +parseInt(1000);
     } else if (salary > 5000 && salary <= 10000) {
-      return salary + 2000;
+      return parseInt(salary) + parseInt(2000);
     } else if (salary > 10000 && salary <= 20000) {
-      return salary + 3000;
+      return parseInt(salary) + parseInt(3000);
     } else if (salary > 20000 && salary <= 50000) {
-      return salary + 4000;
+      return parseInt(salary) + parseInt(4000);
     } else {
       return salary; 
     }
 }
-  let salary = 6000;
+  let salary = prompt("Enter employee salary:-");
   let incrementedSalary = incrementSalary(salary);
   document.write("Incremented Salary is:-"+incrementedSalary);
  
