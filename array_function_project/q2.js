1bd8c45e1f58e479b07a88c9073a9cbe1783abdb
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-19 15:12:48 +0530

    new

---
+++ array_function_project/q2.js
@@ -0,0 +1,4 @@
+let arr = [10,20,30,40,50,60];
+arr.forEach(function(val,i){
+    document.write("index is:"+i+":-"+val+"<br>");
+});
---
+++ array_function_project/q5.js
@@ -0,0 +1,5 @@
+let a = [45,95,68,54,82,16,29,10,34,78];
+document.write(a.sort(function(a,b){ return b-a}));
+for(i=0;i<d.length;i++){
+        document.write(d[i]+"<br>");
+ }
