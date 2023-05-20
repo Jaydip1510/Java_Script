af6fc592aca17ada27b8d0767e6bc4b065f495ae
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-20 17:01:39 +0530

    added

---
+++ string_method/trim.js
@@ -0,0 +1,12 @@
+// let str = "         my name is jaydip makwana                     ";
+// document.write("String legnth is:-"+str.length+"<br>");
+
+//   let str2 = str.trimEnd();
+//   document.write("string is:-"+str2+"<br>");
+//   document.write("string length is:-"+str2.length+"<br>");
+
+  let ch = "4";
+  document.write("<br>after padstart is:-"+ch.padStart(5,"@"));
+  document.write("<br>after padend is:-"+ch.padEnd(6,"$"));
+
+ 
