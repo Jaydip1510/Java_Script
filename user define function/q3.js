a913cb642e9e657a46c6b71c373ebf77a794a215
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-20 09:48:44 +0530

    added

--- user define function/q3.js
+++ user define function/q3.js
@@ -1,17 +1,26 @@
-function meterToFoot(meter) {
-    return meter * 3.281;
-  }
-  
-  function celsiusToFahrenheit(celsius) {
-    return (celsius * 9) / 5 + 32;
-  }
-  
-  function dollarToRupees(dollar) {
-    
-    const conversionRate = 73.99;
-    return dollar * conversionRate;
-  }
-  
-  document.write(meterToFoot(5)); 
-  document.write(celsiusToFahrenheit(25)); 
-  document.write(dollarToRupees(10)); 
+function meter(){
+   let a = prompt("Enter the value:-");
+
+   let b = a*3.280;
+   document.write("Meter to foot convert is:-"+b+"<br>");
+}
+meter();
+
+function celsius(){
+   let celsius = prompt("enter the temperature of celsius:-");
+   let fahrenheit = (celsius * 1.8)+32;
+   celsius = parseFloat(celsius);
+   document.write("celsius to fahrenheit is:-"+fahrenheit+"<br>");
+}
+celsius();
+
+function dollar(){
+  let rupees = prompt("Enter the value:-");
+  let dollar = 82.43;
+  let convert = rupees*dollar;
+  rupees = parseInt(rupees);
+  document.write("doller to rupees is:-"+convert);
+
+}
+dollar();
+
