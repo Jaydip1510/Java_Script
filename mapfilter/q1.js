ea3ddde278c118740c017fa39cb1f8f9b2183aeb
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-22 16:57:19 +0530

    changes

--- mapfilter/q1.js
+++ mapfilter/q1.js
@@ -1,8 +1,6 @@
 let array = [23,33,45,43,12];
 let max = array[0];
 let i;
-//array.sort(function(a,b){return b - a});
-//array[0];
 for(i=0; i<array.length;i++){
     if(array[i]>max){
         max = array[i];
