26adcac8567ac5d54ce9034a711c680a9af07213
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-22 15:27:58 +0530

    added

---
+++ object/emp.js
@@ -0,0 +1,11 @@
+let a = {};
+// let b = new Object();
+let c = {
+    firstname:"jaydip",
+    lastname:"makwana",
+    age:"25"
+};
+
+document.write("Your firstname is:-"+c.firstname+"<br>");
+document.write("Your lastname is:-"+c.lastname+"<br>");
+document.write("Your age is:-"+c.age+"<br>");
