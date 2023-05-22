a6b3f498ceb064a20bfebd1430a4b5d75ddfed01
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-22 13:19:18 +0530

    added

--- mapfilter/q1.js
+++ mapfilter/q1.js
@@ -1,9 +1,15 @@
 let array = [23,33,45,43,12];
 let max = array[0];
 let i;
-for(i=1; i<array.length;i++){
+for(i=0; i<array.length;i++){
     if(array[i]>max){
         max = array[i];
     }
 } 
 document.write(max);
+if(array.length ===0){
+    document.write("maximum no not found");
+}else{
+    document.write("Maximum number found");
+
+}
