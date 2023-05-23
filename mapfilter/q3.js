ff5cb1f6356e30dd8b3cd69ee7edc6cdc3e0e3b9
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-23 10:24:06 +0530

    changes

--- mapfilter/q3.js
+++ mapfilter/q3.js
@@ -1,12 +1,12 @@
 let b = [1, 3, 5, 7, 9];
 
-let doubled = b.map(function(num) {
+let arr1 = b.map(function(num) {
   return num * 2;
 });
 
-let tripled = b.map(function(num) {
+let arr2 = b.map(function(num) {
   return num * 3;
 });
 
-document.write("Array Element is:-"+doubled+"<br>"); 
-document.write("Array Element is:-"+tripled);
+document.write("Array Element is:-"+arr1+"<br>"); 
+document.write("Array Element is:-"+arr2);
