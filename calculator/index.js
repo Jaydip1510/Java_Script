dda25b7fd4ef0ae34d40ba01fe2b0cf40d32062f
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-24 17:45:14 +0530

    added

--- calculator/index.js
+++ calculator/index.js
@@ -1,46 +1,28 @@
-function add() {
-    let a = document.getElementById('no1').value;
-    let c = eval(a);
-    document.getElementById('no1').value = c;
-}
-function sub(){
-    let a = document.getElementById('no1').value;
-    let c = eval(a);
-    document.getElementById('no1').value = c;
-}
-function mul(){
-    let a = document.getElementById('no1').value;
-    let c = eval(a);
-    document.getElementById('no1').value = c;
-}
-function div(){
-    let a = document.getElementById('no1').value;
-    let c = eval(a);
-    document.getElementById('no1').value = c;
-}
-function operation(val)
-{
-
- let operation_code = val.getAttribute("opcode");
- console.log(operation_code);
- if(operation_code == 'cls');
- {
-    document.getElementById('no1').value = '0';
-    document.getElementById('no1').value;
- }
-}
-function btn(val){
+function operation(val) {
+    let opcode = val.getAttribute("opcode");
     let val_no1 = document.getElementById('no1').value;
-    if(val_no1)
-    {
-        if(val_no1.length === 1 && val_no1 == '0')
-        {
-            val_no1  = '';
+    switch (opcode) {
+        case "cls":
+            document.getElementById('no1').value = '0';
+            break;
+        case "/": case "*": case  "-": case  "+":
+            document.getElementById('no1').value = val_no1 + val.getAttribute("opcode");
+            break;
+        case "=":
+            document.getElementById('no1').value = eval(val_no1);
+            break;
+        default:
+            break;
+    }
+}
+function btn(val) {
+    let val_no1 = document.getElementById('no1').value;
+    if (val_no1) {
+        if (val_no1.length === 1 && val_no1 == '0') {
+            val_no1 = '';
         }
         document.getElementById('no1').value = val_no1 + val.getAttribute("opcode");
-    }else
-    {
+    } else {
         document.getElementById('no1').value = val.getAttribute("opcode");
     }
-    
 }
