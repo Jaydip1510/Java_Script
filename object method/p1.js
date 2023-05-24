100748a0d89cb955daebe63731c7bd1d1b5d2f3c
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-24 10:12:23 +0530

    added

---
+++ object method/p1.js
@@ -0,0 +1,18 @@
+let emp = {
+    fname : "jaydip",
+    lname : "makwana",
+    age : "25",
+    fullname : function(){
+       return this.fname+" "+this.lname;
+    },
+    get getdata(){
+           return this.lname.toUpperCase();
+    },
+    set setname(newval){
+         this.fname.newval;
+    }
+};
+document.write("fullname is:-"+emp.fullname()+"<br>");
+document.write("last name is:-"+emp.getdata+"<br>");
+emp.fname = "arjun";
+document.write("update name is:-"+emp.fname);
