56dc6deec0a4b9ccd6baeb7a5014524055ce2318
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-24 10:27:33 +0530

    added

---
+++ task/emp.js
@@ -0,0 +1,19 @@
+let emp = {
+    fname : "jaydip",
+    lname : "makwana",
+    age : "25",
+    fullname : function(){
+        return this.fname+" "+this.lname;
+    },
+    get getdata(){
+        return this.fname.toUpperCase();
+    },
+    set setdata(newval){
+        this.lname = newval;
+    }
+
+};
+document.write("fullname is:-"+emp.fullname()+"<br>");
+document.write("first name is:-"+emp.getdata+"<br>");
+emp.lname = "rawte";
+document.write("last name is:-"+emp.lname);
