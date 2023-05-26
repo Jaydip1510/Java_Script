25e52eac724b6bb7e6c1682c637a59bac94b2b81
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-26 20:19:05 +0530

    added

--- class/emp.js
+++ class/emp.js
@@ -5,12 +5,27 @@ class emp{
              this.age = age;
       }
       displaydata(){
+
           return this.id+"<br>Data is:-"+this.name+"<br>Data is:-"+this.age+"<br>";
+          
       }
             
 }
-let a = new emp(101,"jaydip","25");
-document.write("Data is:-"+a.displaydata());
+let a = new emp();
+let arr = [];
+      function savedata(){
+             var data = document.getElementById('t_name').value;
+             var age = document.getElementById('t_age').value;
+             
+             let id = arr.length; 
+             console.log("Array Length =>"+id);
+             if(id === 0){ id = 1; }else{ id = id + 1;}
+             let obj = new emp(id,data,age); 
+             arr.push(obj)
+             console.log(arr);
+      }
+
+// document.write("Data is:-"+a.displaydata());
 
 emp = class{
      constructor(id,name,age){
@@ -27,5 +42,5 @@ emp.prototype.displaydata = function(){
       return this.id+"---"+this.name+"---"+this.age;
 }
 let obj = new emp(1204,"jaydip",26);
-document.write("disp data is:-"+obj.displaydata());
+//document.write("disp data is:-"+obj.displaydata());
              
