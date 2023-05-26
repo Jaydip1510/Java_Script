03bfe8af6a3bd7de50dcdda8b3fb629b0b62ee9e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-26 17:26:19 +0530

    changes

---
+++ calculator_project/cal.js
@@ -0,0 +1,20 @@
+let txt = '';
+function getval(no){
+    txt +=no;
+    document.getElementById('no1').value = txt;
+
+}
+function getans(){
+    var t1 = document.getElementById('no1').value;
+    var ans = eval(t1);
+    document.getElementById('no1').value = ans;
+    txt = ''; 
+}
+function cleardata(){
+     document.getElementById('no1').value = '';
+     txt = '';
+}
+function bksp(){
+    var v1 = document.getElementById('no1').value;
+    document.getElementById('no1').value = v1.substr(0,v1.length-1);
+}
