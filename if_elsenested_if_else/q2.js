2bae68396fb3217bddee77d4fa29c4ded8294441
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-28 16:34:55 +0530

    changes

--- if_elsenested_if_else/q2.js
+++ if_elsenested_if_else/q2.js
@@ -1,2 +1,2 @@
 let no = prompt("Enter Any Number:-");
-(no%2==0) ? document.write(no+"&nbspis even"):document.write(no+"&nbspno is odd");
+(no%2==0) ? document.write(no+"&nbspis even"):document.write(no+"&nbsp is odd");
