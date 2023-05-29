4887a6f406a3dd9c50b2982878d11a85e77fc556
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-29 15:37:37 +0530

    added

---
+++ practice/event.js
@@ -0,0 +1,21 @@
+function btnclick() {
+    document.getElementById('txt1').value="hello";
+    
+}
+
+function btndblclick(){
+    document.getElementById('txt1').value="how are you";
+    
+}
+function rightclick(){
+    document.getElementById('txt1').value="hello jaydip";
+    
+}
+function mousehoverevent(){
+    document.getElementById('txt1').value="Mouse Hover";
+    
+}
+function mouseleaveevent(){
+    document.getElementById('txt1').value="Mouse Leave";
+    
+}
