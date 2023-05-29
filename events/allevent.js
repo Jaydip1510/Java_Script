aeeccb97553c73dddc8b97c0c841e2b2709a0d17
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-29 15:03:29 +0530

    added

---
+++ events/allevent.js
@@ -0,0 +1,21 @@
+function btnclick() {
+     document.getElementById('txt1').value="hello";
+       
+}
+function btndbclick(){
+    document.getElementById('txt1').value="How Are You?";
+    
+}
+
+function btnrightclick(){
+    document.getElementById('txt1').value="hello jaydip";
+    
+}
+function mouseHoverEvent(){
+    document.getElementById('txt1').value="Mouse hover";
+    
+}
+function mouseleaveEvent(){
+    document.getElementById('txt1').value = "Mouse leave";
+    
+}
