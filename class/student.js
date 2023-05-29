7000a6f494045061e09c08f3990eface30d9f2fe
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-29 16:09:44 +0530

    changes

--- class/student.js
+++ class/student.js
@@ -8,12 +8,14 @@ class student{
        display(){
         document.write('Your rollno is:-'+this.rollno);
         document.write('<br>Your name is:-'+this.name);
-        document.write('<br>Your age is:-'+this.age+"<br>");
+        document.write('<br>Your age is:-'+this.age);
+        document.write('<br>Your salary is:-'+this.salary+"<br>");
+        
         
        }
 
 }
-let s1 = new student("12","jaydip","25");
+let s1 = new student("12","jaydip","25","20000");
 s1.display();
 
  student = class{
@@ -21,11 +23,11 @@ s1.display();
               this.rollno=rollno;
               this.name=name;
               this.age=age;
-              this.salary;
+              this.salary=salary;
        }
 };
 student.prototype.display = function(){
        return this.salary;
 }
-let obj = new student("102","jaydip","25000");
+let obj = new student(14,"jaydip",24,28000);
 document.write("data is:-"+obj.display());
