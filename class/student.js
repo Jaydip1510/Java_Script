c326ba8bf04441849b51131ffbaa98296b9844a2
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-29 16:03:14 +0530

    added

--- class/student.js
+++ class/student.js
@@ -1,13 +1,14 @@
 class student{
-       constructor(rollno,name,age){
+       constructor(rollno,name,age,salary){
         this.rollno=rollno;
         this.name=name;
         this.age=age;
+        this.salary=salary;
        }
        display(){
         document.write('Your rollno is:-'+this.rollno);
         document.write('<br>Your name is:-'+this.name);
-        document.write('<br>Your age is:-'+this.age);
+        document.write('<br>Your age is:-'+this.age+"<br>");
         
        }
 
@@ -16,14 +17,15 @@ let s1 = new student("12","jaydip","25");
 s1.display();
 
  student = class{
-       constructor(rollno,name,age){
+       constructor(rollno,name,age,salary){
               this.rollno=rollno;
               this.name=name;
               this.age=age;
+              this.salary;
        }
 };
 student.prototype.display = function(){
-       return this.id;
+       return this.salary;
 }
-let obj = new student(102,"jaydip");
-document.write('Data is:-'+obj.display());
+let obj = new student("102","jaydip","25000");
+document.write("data is:-"+obj.display());
