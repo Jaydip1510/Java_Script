6d3f4f501698437e9fe701a96740716f725c6157
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-29 15:47:50 +0530

    changes

--- class/student.js
+++ class/student.js
@@ -14,3 +14,16 @@ class student{
 }
 let s1 = new student("12","jaydip","25");
 s1.display();
+
+ student = class{
+       constructor(rollno,name,age){
+              this.rollno=rollno;
+              this.name=name;
+              this.age=age;
+       }
+};
+student.prototype.display = function(){
+       return this.id;
+}
+let obj = new student(102,"jaydip");
+document.write('Data is:-'+obj.display());
