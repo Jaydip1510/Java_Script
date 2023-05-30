0ff8fde16509aef553e03a41c1b88c145e68343a
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-05-30 18:02:57 +0530

    changes

--- events/createevent.js
+++ events/createevent.js
@@ -14,7 +14,7 @@ function customdisp(){
               age : 25
            }
        });
-       document.addEventListener('userInfo',(data)=>{
+        document.addEventListener('userInfo',(data)=>{
         document.write("Your name is:-"+data.detail.name);
         document.write('<br>Your surname is:-'+data.detail.surname);
         document.write('<br>Your Age is:-'+data.detail.age);
