10545e30b2606f58a5a15956f8bc11c67dfac800
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-03 20:32:44 +0530

    changes

--- student_form_project/index.js
+++ student_form_project/index.js
@@ -1,6 +1,8 @@
 function studentform() {
     let sname = document.stuform.sname.value;
     let name = document.stuform.name.value;
+    let age = document.stuform.age.value;
+    let email = document.stuform.email.value;
 
     if(sname == ''){
         document.stuform.sname.focus();
@@ -24,9 +26,26 @@ function studentform() {
         document.stuform.name.focus();
         document.getElementById('errname').innerHTML = "Name Must be >2 charrachters";
         document.getElementById('errname').style.color = 'red';
-        return false;
-        
-        
+        return false;    
     }
-    
+    if(age == ''){
+        document.stuform.age.focus();
+        document.getElementById('errage').innerHTML = "Please Enter Age";
+        document.getElementById('errage').style.color = 'red';
+        return false;
+    }
+    if(age<1 || age>100){
+        document.stuform.age.focus();
+        document.getElementById('errage').innerHTML = "Age Must be 1-100";
+        document.getElementById('errage').style.color = 'red';
+        return false;
+    }
+      var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+      var ans = pattern.test(email);
+      if(ans == false){
+        document.userform.email.focus();
+        document.getElementById('erremail').innerHTML = "Please Vaild Email";
+        document.getElementById('erremail').style.color = 'red';
+        return false;
+      }
 }
