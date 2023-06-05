13896db825fadfa295bf3f79e5c1713167e3c51d
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-05 10:28:22 +0530

    added

---
+++ array_iteration/array.js
@@ -0,0 +1,32 @@
+let a = [5,10,15,8,4,20];
+// let b = a.map(function(i){
+//      return i**3;
+// });
+// document.write(b+"<br>");
+
+let b = a.map((i)=>i**2);
+let txt = '';
+b.forEach(function(val){
+ txt += val+"<br>";
+});
+document.getElementById('demo').innerHTML = txt;
+
+let c = a.filter(function(i){
+     return i%2 == 0;
+});
+c.forEach(function(v){
+    document.write(v+"<br>");
+});
+
+let d = a.reduceRight(function(t,v){
+    return t+v;
+});
+document.write('<br>Total is:-'+d+"<br>");
+if(a.includes(50)){
+    document.write('value found'); 
+}else{
+    document.write('no value found');
+    
+}
+
+
