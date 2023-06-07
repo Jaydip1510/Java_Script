f9117d856fea8b5a30ea6089390f1b2f847fb2dc
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-07 15:34:35 +0530

    changes

--- array_iteration/array.js
+++ array_iteration/array.js
@@ -1,15 +1,15 @@
 let a = [5,10,15,8,4,20];
-// let b = a.map(function(i){
-//      return i**3;
-// });
-// document.write(b+"<br>");
-
-let b = a.map((i)=>i**2);
-let txt = '';
-b.forEach(function(val){
- txt += val+"<br>";
+let b = a.map(function(i){
+     return i**3;
 });
-document.getElementById('demo').innerHTML = txt;
+document.write(b+"<br>");
+
+// let b = a.map((i)=>i**2);
+// let txt = '';
+// b.forEach(function(val){
+//  txt += val+"<br>";
+// });
+// document.getElementById('demo').innerHTML = txt;
 
 let c = a.filter(function(i){
      return i%2 == 0;
