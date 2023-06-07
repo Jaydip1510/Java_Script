e8b312208b891005968c85e2fb82ea6e6d2fcbe9
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-07 14:22:10 +0530

    added

---
+++ scope/arow.js
@@ -0,0 +1,19 @@
+function abc(){
+      return jaydip;
+}
+
+let a = () => {
+     return 2 * 2;
+};
+document.write('a is:-'+a()+"<br>");
+
+let b = (i) => i*i*i;
+document.write('cube is:-'+b(5)+"<br>");
+
+let c = (a,b,c=5) => a+b*c;
+document.write('cube is:-'+c(2,5,7)+"<br>");
+
+let ans = b(4) + 10;
+document.write('ans is:-'+ans);
+
+
