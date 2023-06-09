b548f63e0d4b1621c79eb1a74256509512ee9a09
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-09 09:56:54 +0530

    added

---
+++ class/add.js
@@ -0,0 +1,13 @@
+class add{
+    constructor(a,b){
+     this.a=a;
+     this.b=b;
+    }
+     addition(){
+        document.write('addition is:-'+this.a+""+this.b);
+        
+     }
+
+};
+let a1 = new add("20","25");
+a1.addition();
