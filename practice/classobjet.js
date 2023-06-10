bc73756aebefb79cbd91126dca3827be88bde2c9
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-10 17:01:15 +0530

    added

---
+++ practice/classobjet.js
@@ -0,0 +1,18 @@
+let user = {
+    uname : "jaydip",
+    surname : "makwana",
+    age : 25,
+    salary : 25000,
+    qualification : ["10th","12th","bca","mca"],
+
+    display(){
+        document.write('user name is:-'+user.uname+"<br>");
+        document.write('surname is:-'+user.surname+"<br>");
+        document.write('age is:-'+user.age+"<br>");
+        document.write('salary is:-'+user.salary);
+        
+        
+        
+    }
+};
+user.display();
