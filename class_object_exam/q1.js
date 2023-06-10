a1d5ba94f73bc0d18d498c2166b53f5544dcabc8
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-10 11:20:35 +0530

    changes

--- class_object_exam/q1.js
+++ class_object_exam/q1.js
@@ -9,8 +9,7 @@ let user = {
         document.write('username is:-'+user.username+"<br>");
         document.write('surname is:-'+user.surname+"<br>");
         document.write('Age is:-'+user.age+"<br>");
-        document.write('salary is:-'+user.salary+"<br>");
-        // document.write('qulification is:-'+user.qualification);     
+        document.write('salary is:-'+user.salary+"<br>"); 
         
         for(let i=0; i<this.qualification.length;i++){
             document.write(''+this.qualification[i]+"<br>");
