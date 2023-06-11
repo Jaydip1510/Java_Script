3811f132a422fbfbb0c3d70cb5954173dc37f54e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-11 10:42:36 +0530

    added

--- class/emp.js
+++ class/emp.js
@@ -20,7 +20,40 @@ function editdata(index) {
       document.getElementById('t_name').value = arr[index].name;
       document.getElementById('t_age').value = arr[index].age;
       edit_position = index;
-}
+      var tbl = '<table id="tblData" border="1" align="center">';
+      tbl += '<tr>';
+      tbl += '<td width="15%">Id</td>';
+      tbl += '<td width="60%">Name</td>';
+      tbl += '<td width="15%">Age</td>';
+      tbl += '<td width="10%">Operation</td>';
+      tbl += '</tr>';
+
+      arr.forEach(a => {
+            
+            var index = a.id - 1;
+            let opration_html = '<a href="#" onclick="editdata(' + index + ')">Edit</a>';
+            let trHtml = '<tr>';
+            if(index === edit_position)
+            {
+                  trHtml += '<td><mark>' + a.id +'</mark></td>';
+                  trHtml += '<td><mark>' + a.name +'</mark></td>';
+                  trHtml += '<td><mark>' + a.age + '</mark></td>';
+                  trHtml += '<td><mark>' + opration_html + '</mark></td>';
+            }else{
+                  trHtml += '<td>' + a.id + '</td>';
+                  trHtml += '<td>' + a.name + '</td>';
+                  trHtml += '<td>' + a.age + '</td>';
+                  trHtml += '<td>' + opration_html + '</td>';
+            }
+            
+            
+            trHtml += '</tr>';
+            tbl = tbl + trHtml;
+      });
+      tbl = tbl + '</table>';
+      document.getElementById("outPut").innerHTML = tbl;
+      }
+
 function savedata(a) {
       var data = document.getElementById('t_name').value;
       var age = document.getElementById('t_age').value;
@@ -59,6 +92,8 @@ function savedata(a) {
             trHtml += '<td>' + opration_html + '</td>';
             trHtml += '</tr>';
             tbl = tbl + trHtml;
+            
+            
       });
       tbl = tbl + '</table>';
       document.getElementById("outPut").innerHTML = tbl;
