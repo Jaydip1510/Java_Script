96ee320c34a0d4f51794510b6f67a83df1489ed0
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-12 14:34:04 +0530

    changes

--- dom_elements/domelement.js
+++ dom_elements/domelement.js
@@ -7,3 +7,17 @@ for(let i=0;i<a.length;i++){
 }
 let b = document.getElementsByClassName('box');
 document.getElementById('uimg').src="natural.jpg";
+document.getElementById('txt1').innerHTML = "<table border='1'><tr><td>jaydip</td></tr><tr><td>raj</td></tr>";
+// document.getElementById('btn1').onclick = function(){
+//     alert("helllo hiii....");
+// }
+// document.getElementById('btn1').ondblclick = function(){
+//     alert('Double click pressed..');
+    
+// }
+
+document.getElementById('btn1').addEventListener('click',disp);
+function disp(){
+    alert('Event called...')
+}
+
