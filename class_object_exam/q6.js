015a883cd7db9eb764519397ae600f131a45275e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-12 16:41:08 +0530

    changes

--- class_object_exam/q6.js
+++ class_object_exam/q6.js
@@ -1,7 +1,7 @@
 function greet(name = "jaydip") {
     document.write(`Hello ${name}`+"<br>");
   }
-  // Calling the function without any arguments
+// Calling the function without any arguments
 greet();
   // Calling the function with an argument
   greet("how are you");
