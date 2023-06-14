d56810c2ec432ce920d9220e49a462650480ab28
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-14 19:25:45 +0530

    changes

--- Localstorage/localstorage_form.js
+++ Localstorage/localstorage_form.js
@@ -1,26 +1,41 @@
+let arr = [];
 function insertData(){
-    var uname = document.userform.uname.value;
-    var age = document.userform.age.value;
+    let obj = {
+         uname : "",
+         age : "",
+         sal : ""
+    };
+    var uname  = document.userform.uname.value;
+    var age    = document.userform.age.value;
     var salary = document.userform.sal.value;
-    let user = [uname,age,salary];
-      localStorage.setItem("username",uname);
-      localStorage.setItem("age",age);
-      localStorage.setItem("salary",salary);
+      
+      obj.uname = uname;
+      obj.age = age;
+      obj.sal = salary;
+
+      arr.push(obj);
+      
+      localStorage.setItem("arrDetail",JSON.stringify(arr));
     dispData();
 }
 function dispData(){
-    let uname = localStorage.getItem("username");
-    let age = localStorage.getItem("age");
-    let sal = localStorage.getItem("salary");
-    document.getElementById("userData").innerHTML = "<tr><th>Name</th><th>Age</th><th>Salary</th><th>Action</th></tr><tr><td>"+uname+"</td><td>"+age+"</td><td>"+sal+"</td><td><button type='button' id='btnDel' name='btnDel' onclick='delData()'>Delete</button></td></tr>";
+    let arrDetail = localStorage.getItem("arrDetail");
+    let JsonData = JSON.parse(arrDetail);
+    let html = "";
+    html += "<tr><th>Name</th><th>Age</th><th>Salary</th><th>Action</th></tr>";
+    JsonData.forEach( (a,b) => {
+        html += "<tr><td>"+a.uname+"</td><td>"+a.age+"</td><td>"+a.sal+"</td>";
+        html +="<td><button type='button' id='btnDel' name='btnDel' onclick='delData()'>Delete</button></td></tr>";
+
+    });
+    
+    document.getElementById("userData").innerHTML = html;
     document.userform.uname.value = '';
     document.userform.age.value = '';
     document.userform.sal.value = '';
 }
 function delData(){
-    localStorage.removeItem('username');
-    localStorage.removeItem('age');
-    localStorage.removeItem('salary');
+    localStorage.removeItem('arrDetail');
     document.getElementById('userData').innerHTML = '';
     document.getElementById('msg').innerHTML= "user Successfully deleted....";   
 }
