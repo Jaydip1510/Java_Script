4e08ed61d1ba965b64ad943577ee93ba592bba7f
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-23 18:47:07 +0530

    changes

--- javascript_website/index.js
+++ javascript_website/index.js
@@ -1,4 +1,5 @@
 var cartArray = [];
+const gc_local_storage = 'CartDetail';
 window.addEventListener("DOMContentLoaded", (event) => {
 
     var items = document.getElementsByClassName("add-to-cart");
@@ -6,7 +7,64 @@ window.addEventListener("DOMContentLoaded", (event) => {
         items[i].addEventListener('click', fun);
     }
 });
-  
+function cartTable()
+{
+    var count = 0;
+    var btnCartCount =  document.getElementById("cart");
+    var span_tag = btnCartCount.getElementsByTagName('span')[0];
+
+    if(cartArray.length <= 0)
+    {
+        localStorage.removeItem(gc_local_storage);
+    
+      let arrDetail = localStorage.getItem(gc_local_storage);
+         document.getElementById('staticBackdrop_body').innerHTML = '';
+         span_tag.textContent = count;
+         return true;
+     }
+    let html = "<table class='table table-responsive'>";
+    html+= "<thead>";
+    html+= "<tr>";
+    html+="<th scope='col'>Product Image</th>";
+    html+="<th scope='col'>Product Name</th>";
+    html+="<th scope='col'>Product price</th>";
+    html+="<th scope='col'>qty</th>";
+    html+="<th scope='col'>Sub total</th>";
+    html+="<th scope='col'>Action</th>";
+    html+="</tr>";
+    html+="</thead>";
+    var tot_price = 0,
+         tot_qty  = 0;
+
+    for(let i in cartArray){
+        count = count + cartArray[i].qty;
+         //for(let i=0;i<cartArray.length;i++){
+        let subtot = 0;
+        subtot = parseInt(cartArray[i].price) * parseInt(cartArray[i].qty);
+        tot_qty = tot_qty + parseInt(cartArray[i].qty);
+      html+="<tr>";
+      html+="<td><img src='"+cartArray[i].img+"' height='100' width='100'/></td>";
+      html+="<td>"+cartArray[i].pname+"</td>";
+      html+="<td>"+cartArray[i].price+"</td>";
+      html+="<td>"+cartArray[i].qty+"</td>";
+      html+="<td>"+subtot+"</td>";
+      html+="<td><input type='button' name='del' id='del' value='Delete' onclick= 'delData("+i+")'>";
+      html+="</tr>";
+      tot_price = tot_price + subtot;
+    } 
+    html+="<tr>";
+      html+="<td colspan='3'>Total</td>";
+      html+="<td>"+tot_qty+"</td>";
+      html+="<td>"+tot_price+"</td>";
+      html+="</tr>";
+      html+="</html>";
+      span_tag.textContent = count;
+   
+      localStorage.setItem(gc_local_storage,JSON.stringify(cartArray));
+    
+      let arrDetail = localStorage.getItem(gc_local_storage);
+    document.getElementById('staticBackdrop_body').innerHTML = html;
+}
 function fun() 
 {
     var cartObj = {"pname":"", "price":"", "qty":1,"img":""};
@@ -37,84 +95,18 @@ function fun()
     {
         cartArray.push(cartObj);
     }
-
-    updateCartCount();
-    let html = "<table class='table table-responsive'>";
-    html+= "<thead>";
-    html+= "<tr>";
-    html+="<th scope='col'>Product Image</th>";
-    html+="<th scope='col'>Product Name</th>";
-    html+="<th scope='col'>Product price</th>";
-    html+="<th scope='col'>qty</th>";
-    html+="<th scope='col'>Sub total</th>";
-    html+="<th scope='col'>Action</th>";
-    html+="</tr>";
-    html+="</thead>";
-    var tot_price = 0,
-         tot_qty  = 0;
-
-    for(let i=0;i<cartArray.length;i++){
-        let subtot = 0;
-        subtot = parseInt(cartArray[i].price) * parseInt(cartArray[i].qty);
-        tot_qty = tot_qty + parseInt(cartArray[i].qty);
-      html+="<tr>";
-      html+="<td><img src='"+cartArray[i].img+"' height='100' width='100'/></td>";
-      html+="<td>"+cartArray[i].pname+"</td>";
-      html+="<td>"+cartArray[i].price+"</td>";
-      html+="<td>"+cartArray[i].qty+"</td>";
-      html+="<td>"+subtot+"</td>";
-      html+="<td><input type='button' name='del' id='del' value='Delete' onclick= 'delData()'>";
-      html+="</tr>";
-      tot_price = tot_price + subtot;
-    } 
-    html+="<tr>";
-      html+="<td colspan='3'>Total</td>";
-      html+="<td>"+tot_qty+"</td>";
-      html+="<td>"+tot_price+"</td>";
-      html+="</tr>";
-      html+="</html>";
-    document.getElementById('staticBackdrop_body').innerHTML = html;
-
-    let arr = [];
-    let obj = {
-        pname:pname,
-        price:price,
-        qty:1
-    };
-    obj.pname= pname;
-    obj.price=price;
-    obj.qty=1;
-
-        arr.push(obj);
-        localStorage.setItem("arrDetail",JSON.stringify(arr));
+    cartTable();
     
-        let arrDetail = localStorage.getItem("arrDetail");
+   
+
+ 
 
 }
-
-
-function updateCartCount()
-{
-    var count = 0;
-    var btnCartCount =  document.getElementById("cart");
-    var span_tag = btnCartCount.getElementsByTagName('span')[0];
-    for(var i = 0;i < cartArray.length; i++)
-    {
-        count = count + cartArray[i].qty;
-    }
-    span_tag.textContent = count;
-
-}
-
-function delData(){
-        cartArray=['abc'];
-        cartArray.splice(0,1);
-
-    delete html().splice(0,1,);
-    for(let i=0;i<arrDetail.length;i--){
-        alert('sfjdslk');
-        console.log(arrDetail[i]);
-    }
+function delData( del_idx ){
+        cartArray.splice(del_idx, 1);
+        console.log(cartArray);
+        cartTable();
+       
 
 }
 
