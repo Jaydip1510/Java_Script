8bd3e2fc517f386dd2df9f017c60d02f080fc7a9
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-26 17:37:37 +0530

    changes

--- extra work/localstorage.js
+++ extra work/localstorage.js
@@ -5,7 +5,7 @@ document.getElementById("b1").addEventListener("click", () => {
     let bdate = document.uform.bdate.value;
     let salary = document.uform.sal.value;
     let gen = document.uform.g1.value;
-
+    let uid = document.uform.userid.value;
     let data1 = {};
     let user = {
         name: name,
@@ -17,13 +17,27 @@ document.getElementById("b1").addEventListener("click", () => {
 
     let datainfo = JSON.parse(localStorage.getItem("userInfo"));
     if (datainfo != null) {
-        datainfo.push(user);
+        if(uid != ''){
+          for(let i = 0; i<datainfo.length;i++){
+            if(uid == i){
+                datainfo[i].name = name;
+                datainfo[i].age = age;
+                datainfo[i].birthdate = bdate;
+                datainfo[i].salary = sal;
+                datainfo[i].gender = gen;
+            }
+          }
+        }else{
+            datainfo.push(user);
+        }
+        
         localStorage.setItem("userInfo", JSON.stringify(datainfo));
     } else {
         data1 = [user];
         localStorage.setItem("userInfo", JSON.stringify(data1));
     }
     document.uform.reset();
+    document.uform.uid.value = '';
     display();
 });
 
@@ -48,7 +62,8 @@ function display() {
             dt += "<td><center>" + user[i].birthdate + "</center></td>";
             dt += "<td><center>" + user[i].salary + "</center></td>";
             dt += "<td><center>" + user[i].gender + "</center></td>";
-            dt += "<td><center><input type='button' name='btndel' id='btndel' value='Delete' onclick = 'delData(" + i + ")'></center></td>";
+            dt += "<td><center><input type='button' name='btndel' id='btndel' value='Delete' onclick = 'delData(" + i + ")'>";
+            dt += "   <input type='button' name='edit' id='edit' value='Edit' onclick = 'dataEdit("+i+")'</center></td>";
             dt += "</tr>";
         }
     }
@@ -60,4 +75,20 @@ function delData(id){
     localStorage.setItem("userInfo",JSON.stringify(user));
     display();
 
+}
+
+function dataEdit(id){
+    let datainfo = JSON.parse(localStorage.getItem("userInfo"));
+    if(datainfo != null){
+        for(let i = 0; i<datainfo.length;i++){
+            if(i == id){
+                document.uform.ename.value = datainfo[i].name;
+                document.uform.age.value = datainfo[i].age;
+                document.uform.bdate.value = datainfo[i].birthdate;
+                document.uform.sal.value = datainfo[i].salary;
+                document.uform.g1.value = datainfo[i].gender;
+
+            }
+        }
+    } 
 }
