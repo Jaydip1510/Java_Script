88c27c7ef912fff5ffc19173d8129e536727b14c
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-27 18:02:20 +0530

    changes

---
+++ events/task.js
@@ -0,0 +1,40 @@
+function add(){
+    let a = document.getElementById('no1').value;
+    let b = document.getElementById('no2').value;
+    let c = parseInt(a)+parseInt(b);
+    document.getElementById('ans').value = c;
+}
+
+function sub(){
+    let a = document.getElementById('no1').value;
+    let b = document.getElementById('no2').value;
+    let c = a-b;
+    document.getElementById('ans').value = c;
+}
+
+function mul(){
+    let a = document.getElementById('no1').value;
+    let b = document.getElementById('no2').value;
+    let c = a*b;
+    document.getElementById('ans').value = c; 
+}
+function div(){
+    let a = document.getElementById('no1').value;
+    let b = document.getElementById('no2').value;
+    let c = a/b;
+    document.getElementById('ans').value = c; 
+}
+
+function cleardata(){
+    document.getElementById('no1').value = '';
+    document.getElementById('no2').value = '';
+    document.getElementById('ans').value = '';
+}
+
+function firstpage(){
+    window.location.href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox';
+}
+
+function pageprint(){
+    window.print();
+}
