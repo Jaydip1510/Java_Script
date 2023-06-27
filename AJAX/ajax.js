2d8fb234d99e6d9003f6d3f657da1a522e0f4277
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-27 15:50:38 +0530

    changes

---
+++ AJAX/ajax.js
@@ -0,0 +1,12 @@
+document.getElementById("btn1").addEventListener("click", ()=>{
+       let obj = new XMLHttpRequest();
+
+       obj.onreadystatechange = function(){
+        if(this.status == 200){
+            document.getElementById("demo1").innerHTML = this.responseText;
+        }
+       }
+       obj.open("GET","jstext.txt",true);
+       obj.send();
+       console.log(obj);
+});
