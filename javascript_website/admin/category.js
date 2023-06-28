e2e5ed1e71b385d28ab4ea66a65c22b23ec4ffe8
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-28 13:06:08 +0530

    changes

--- javascript_website/admin/category.js
+++ javascript_website/admin/category.js
@@ -1,11 +1,15 @@
 document.getElementById("catdata").addEventListener("click",()=>{
     let catname = document.catform.catname.value;
     let catdata = {};
+    let category = {
+        id:id,
+        name:catname
+    }
     let catinfo =  localStorage.setItem("categorydata",JSON.stringify(catname));
     if(catname != null){
-        catinfo.push(catdata);
-        
+        catinfo.push(category);
+        let cat = catinfo;
     }else{
-
+         
     }
 })
