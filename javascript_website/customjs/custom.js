9c2313e2b2d530e9abe0c06b35fff2de680fb042
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-28 16:44:32 +0530

    changes

--- javascript_website/customjs/custom.js
+++ javascript_website/customjs/custom.js
@@ -2,6 +2,6 @@ let userdata = localStorage.getItem("categoryData");
 let userdata1 = {};
 let row = '';
 for(let i=0; i<userdata.category.length;i++){
-   row += "<li><a class='dropdown-item' href='#'>Notification Preferences</a></li>";
+   row += "<li><a class='dropdown-item' href='#'>"+userdata.category[i].catname+"</a></li>";
 }
 document.getElementById('catid').innerHTML = row;
