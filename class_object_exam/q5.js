2991a082fad8fd9bf3b297c70b6028c4bf520e4e
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-28 09:38:27 +0530

    chages

--- class_object_exam/q5.js
+++ class_object_exam/q5.js
@@ -1,7 +1,7 @@
 function add(txt){
      ch = txt.value.slice(txt.value.length-1);
      if(!((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))){
-        document.getElementById('t1').value = txt.value.slice(0,1);
+        document.getElementById('t1').value = txt.value.slice(0,-1);
      }else{
        
      }
