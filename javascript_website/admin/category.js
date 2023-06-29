3ad4d4e47e222e1d7291d12cbbec192d10e95270
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-06-29 13:19:49 +0530

    changes

--- javascript_website/admin/category.js
+++ javascript_website/admin/category.js
@@ -1,3 +1,4 @@
+
 document.getElementById("catdata").addEventListener("click",()=>{
     let cat = document.catform.catname.value;
     let getdata = localStorage.getItem("categoryData");
@@ -21,13 +22,55 @@ document.getElementById("catdata").addEventListener("click",()=>{
         localStorage.setItem("categoryData",JSON.stringify(data)); 
     }
     document.catform.reset();
-});
+     display();
 
-document.getElementById('DelData').addEventListener("click",()=>{
-    let getdata = localStorage.getItem("categoryData");
-    if(getdata != null){
-    getdata = JSON.parse(getdata);
-    getdata.category.splice(1,1);
-    localStorage.setItem("categoryData",JSON.stringify(getdata));
-}
 });
+function display(){
+    let d1 = '';
+
+    let d2 = JSON.parse(localStorage.getItem("categoryData"));
+
+    if(d2 != null){
+        d1 +="<tr>";
+        d1 +="<th>ID</th>";
+        d1 +="<th>Name</th>";
+        d1 +="<th>Action</th>";
+        d1 +="</tr>";
+
+           for(i = 0; i<d2.category.length;i++){
+            d1 += "<tr>";
+            d1 += "<td>"+d2.category[i].id+"</td>";
+            d1 += "<td>"+d2.category[i].name+"</td>";
+            d1 +="<td><center><input type='button' name='catdel' id='catdel' value='Delete' onclick='delCat("+i+")'>";
+            d1 +="   <input type='button' name='catedit' id='catedit' value='Edit' onclick='editCat("+i+")'></td>";
+            d1 +="</tr></center>";
+        
+        }
+    }
+    document.getElementById('tbl').innerHTML = d1;
+}
+
+function delCat(id) {
+    let user = localStorage.getItem("categoryData");
+    if(user != null){
+        let disp = JSON.parse(user);
+        disp.category.splice(id,1);
+        localStorage.setItem("categoryData",JSON.stringify(disp));
+        display();
+    }
+}
+
+function editCat(id){
+    let editdata = localStorage.getItem("categoryData");
+    let data = JSON.parse(editdata);
+    
+       for(let i = 0; i<data.category.length;i++){
+        if(id == i){
+            
+                document.catform.catname.value = data.category[i].name;
+                document.catform.uid.value = i;
+        }
+       } 
+    }
+
+
