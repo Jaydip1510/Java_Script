34db13f7e251d1b0f92e6dd91d64657e7b58697d
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-07-02 09:47:54 +0530

    changes

--- javascript_website/index.js
+++ javascript_website/index.js
@@ -2,41 +2,46 @@ var cartArray = [];
 const gc_local_storage = 'CartDetail';
 window.addEventListener("DOMContentLoaded", (event) => {
     fetch("./product.json")
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        var row = 0;
-        var html = '';
-    for (var i = 0; i < data.length; i++) {
-        if(row == 0)
-        {
-            html +='<div class="row">';
-        }
-        html +='<div class="col-md-3 col-sm-6">';
-        html +='<div class="product-grid4">';
-        html +='<div class="product-image4">';
-        html +='<a href="#">';
-        html +='<img class="pic-1" src="'+data[i].product_img+'"  height="100" width="100">';
-        html +='</a>';                   
-        html +='</div>';
-        html +='<div class="product-content">';
-        html +='<h3 class="title"><a href="#">'+data[i].product_name+'</a></h3>';
-        html +='<div class="price">'+data[i].product_price+'</div>';
-        html +='<a class="add-to-cart" href="">ADD TO CART</a>';
-        html +='</div>';
-        html +='</div>';
-        html +='</div>';
-       
-        row++;
-        if(row == 4)
-        {
-            html +='</div>';
-            row = 0;
-        }
-    }
-    document.getElementById('product_page').innerHTML = html;
-    });
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            var row = 0;
+            var html = '';
+            for (var i = 0; i < data.length; i++) {
+                if (row == 0) {
+                    html += '<div class="container-fluid">';
+                    html += '<div class="product-list">';
+                    html += '<div class="row">';
+                }
+                html += '<div class="col-md-3 col-sm-6">';
+                html += '<div class="white-box">';
+                html += '<div class="wishlist-icon">';
+                html += '<a href="javascript:void(0);"><span class="badge badge-success">'+data[i].catogory_name+'</span></a>';
+                html += '</div>';
+                html += '<div class="product-img">';
+                html += '<img class="pic-1" src="' + data[i].product_img + '"  height="150" width="150">';
+                html += '</div>';
+                html += '<div class="product-bottom">';
+                html += '<div class="product-name">' + data[i].product_name + '</div>';
+                html += '<div class="price">';
+                html += '<span class="rupee-icon">₹</span>';
+                html += '<span class="product-price">'+ data[i].product_price + '</sapn>';
+                html += '</div>';
+               html += '<button class="btn btn-primary add-to-cart" onClick="add_to_cart(this)">Add To Card</button>';
+                html += '</div>';
+                html += '</div>';
+                html += '</div>';
+                row++;
+                if (row == 4) {
+                    html += '</div>';
+                    html += '</div>';
+                    html += '</div>';
+                    row = 0;
+                }
+            }
+            document.getElementById('product_page').innerHTML = html;
+        });
 
     /** Temp Logic Start
     var Carditems = document.getElementsByClassName("card");
@@ -68,111 +73,104 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
     console.log(final_array);
     /** Temp Logic End */
-    var items = document.getElementsByClassName("add-to-cart");
+    
+   /* -- Not Required For New Logic -- 
+   var items = document.getElementsByClassName("add-to-cart");
     for (var i = 0; i < items.length; i++) {
         items[i].addEventListener('click', fun);
     }
+    */
 });
-function cartTable()
-{
+function cartTable() {
     var count = 0;
-    var btnCartCount =  document.getElementById("cart");
+    var btnCartCount = document.getElementById("cart");
     var span_tag = btnCartCount.getElementsByTagName('span')[0];
 
-    if(cartArray.length <= 0)
-    {
+    if (cartArray.length <= 0) {
         localStorage.removeItem(gc_local_storage);
-    
-      let arrDetail = localStorage.getItem(gc_local_storage);
-         document.getElementById('staticBackdrop_body').innerHTML = '';
-         span_tag.textContent = count;
-         return true;
-     }
-    let html = "<table class='table table-responsive'>";
-    html+= "<thead>";
-    html+= "<tr>";
-    html+="<th scope='col'>Product Image</th>";
-    html+="<th scope='col'>Product Name</th>";
-    html+="<th scope='col'>Product price</th>";
-    html+="<th scope='col'>qty</th>";
-    html+="<th scope='col'>Sub total</th>";
-    html+="<th scope='col'>Action</th>";
-    html+="</tr>";
-    html+="</thead>";
-    var tot_price = 0,
-         tot_qty  = 0;
 
-    for(let i in cartArray){
+        let arrDetail = localStorage.getItem(gc_local_storage);
+        document.getElementById('staticBackdrop_body').innerHTML = '';
+        span_tag.textContent = count;
+        return true;
+    }
+    let html = "<table class='table table-responsive'>";
+    html += "<thead>";
+    html += "<tr>";
+    html += "<th scope='col'>Product Image</th>";
+    html += "<th scope='col'>Product Name</th>";
+    html += "<th scope='col'>Product price</th>";
+    html += "<th scope='col'>qty</th>";
+    html += "<th scope='col'>Sub total</th>";
+    html += "<th scope='col'>Action</th>";
+    html += "</tr>";
+    html += "</thead>";
+    var tot_price = 0,
+        tot_qty = 0;
+
+    for (let i in cartArray) {
         count = count + cartArray[i].qty;
-         //for(let i=0;i<cartArray.length;i++){
+        //for(let i=0;i<cartArray.length;i++){
         let subtot = 0;
         subtot = parseInt(cartArray[i].price) * parseInt(cartArray[i].qty);
         tot_qty = tot_qty + parseInt(cartArray[i].qty);
-      html+="<tr>";
-      html+="<td><img src='"+cartArray[i].img+"' height='100' width='100'/></td>";
-      html+="<td>"+cartArray[i].pname+"</td>";
-      html+="<td>"+cartArray[i].price+"</td>";
-      html+="<td>"+cartArray[i].qty+"</td>";
-      html+="<td>"+subtot+"</td>";
-      html+="<td><input type='button' name='del' id='del' value='Delete' onclick= 'delData("+i+")'>";
-      html+="</tr>";
-      tot_price = tot_price + subtot;
-    } 
-    html+="<tr>";
-      html+="<td colspan='3'>Total</td>";
-      html+="<td>"+tot_qty+"</td>";
-      html+="<td>"+tot_price+"</td>";
-      html+="</tr>";
-      html+="</html>";
-      span_tag.textContent = count;
-   
-      localStorage.setItem(gc_local_storage,JSON.stringify(cartArray));
-    
-      let arrDetail = localStorage.getItem(gc_local_storage);
+        html += "<tr>";
+        html += "<td><img src='" + cartArray[i].img + "' height='100' width='100'/></td>";
+        html += "<td>" + cartArray[i].pname + "</td>";
+        html += "<td>" + cartArray[i].price + "</td>";
+        html += "<td>" + cartArray[i].qty + "</td>";
+        html += "<td>" + subtot + "</td>";
+        html += "<td><input type='button' name='del' id='del' value='Delete' onclick= 'delData(" + i + ")'>";
+        html += "</tr>";
+        tot_price = tot_price + subtot;
+    }
+    html += "<tr>";
+    html += "<td colspan='3'>Total</td>";
+    html += "<td>" + tot_qty + "</td>";
+    html += "<td>" + tot_price + "</td>";
+    html += "</tr>";
+    html += "</html>";
+    span_tag.textContent = count;
+
+    localStorage.setItem(gc_local_storage, JSON.stringify(cartArray));
+
+    let arrDetail = localStorage.getItem(gc_local_storage);
     document.getElementById('staticBackdrop_body').innerHTML = html;
 }
-function fun() 
+function add_to_cart(btn)
 {
-    var cartObj = {"pname":"", "price":"", "qty":1,"img":""};
-    let parentDiv = this.parentNode;
-    let per_parentDiv = parentDiv.parentNode;
+    var cartObj = { "pname": "", "price": "", "qty": 1, "img": "" };
+    let parentDiv       = btn.parentNode;
+    let per_parentDiv   = parentDiv.parentNode;
     
-    var firstParagraph = parentDiv.getElementsByTagName('p')[0];
-    var span_tag = parentDiv.getElementsByTagName('span')[0];
-    var img = per_parentDiv.getElementsByTagName('img')[0];
-    
-    var pname = firstParagraph.textContent;
-    var price = span_tag.textContent;
+    var img_res         = per_parentDiv.getElementsByTagName('img')[0];
+    var prod_name_div   = per_parentDiv.getElementsByClassName('product-name')[0];
+    var prod_price_span = per_parentDiv.getElementsByClassName('product-price')[0];
+  
+    var pname = prod_name_div.textContent;
+    var price = prod_price_span.textContent;
+    var img_src = img_res.src;
     cartObj.pname = pname;
     cartObj.price = price;
-    cartObj.img   = img.src;
-    if(cartArray.length > 0)
-    {
+    cartObj.img   = img_src;
+    if (cartArray.length > 0) {
         var foundValue = cartArray.findIndex(item => item.pname === pname);
-        if(foundValue === -1)
-        {
+        if (foundValue === -1) {
             cartArray.push(cartObj);
 
-        }else
-        {
-            cartArray[foundValue].qty += 1; 
-        }   
-    }else
-    {
+        } else {
+            cartArray[foundValue].qty += 1;
+        }
+    } else {
         cartArray.push(cartObj);
     }
     cartTable();
-    
-   
-
- 
-
 }
-function delData( del_idx ){
-        cartArray.splice(del_idx, 1);
-        console.log(cartArray);
-        cartTable();
-       
+function delData(del_idx) {
+    cartArray.splice(del_idx, 1);
+    console.log(cartArray);
+    cartTable();
+
 
 }
 
