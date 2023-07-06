ad3efaeab38b7a6ec00ad4f2a5560c9070e8d6b7
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-07-06 15:17:03 +0530

    changes

--- task/stud.js
+++ task/stud.js
@@ -1,48 +1,49 @@
 display();
-document.getElementById('btn').addEventListener("click",()=>{
+document.getElementById('pqr').addEventListener("click", () => {
     let name = document.studform.sname.value;
     let age = document.studform.age.value;
     let sal = document.studform.sal.value;
     let gen = document.studform.g1.value;
 
-    let s1 = {};
-
+    let data = {};
     let sdata = {
-        name:name,
-        age:age,
-        salary:sal,
-        gender:gen
+        name: name,
+        age: age,
+        salary: sal,
+        gender: gen
     };
     let sinfo = JSON.parse(localStorage.getItem("studentInfo"));
-     if(sinfo != null){
-        sinfo.push(sdata);
-        localStorage.setItem("studentInfo",JSON.stringify(sinfo));
-    }else{
-       s1 = [sdata];
-       localStorage.setItem("studentInfo",JSON.stringify(s1));
+    if (sinfo != null) {
+        sinfo.studInfo.push(sdata);
+        localStorage.setItem("studentInfo", JSON.stringify(sinfo));
+    } else {
+        data.studInfo = [sdata];
+        localStorage.setItem("studentInfo", JSON.stringify(data));
     }
     document.studform.reset();
     display();
 
 });
-function display(){
-  let sd ="<tr>";
-    sd +="<th>Name</th>";
-    sd +="<th>Age</th>";
-    sd +="<th>Salary</th>";
-    sd +="<th>Gender</th>";
-    sd +="</tr>";
-     let st = localStorage.getItem("studentInfo");
-     if(st !=null){
-       let stud = JSON.parse(st); 
-        for(let i=0; i<stud.length;i++){
-            st +="<tr>";
-            st +="<td>"+stud[i].name+"</td>";
-            st +="<td>"+stud[i].age+"</td>";
-            st +="<td>"+stud[i].salary+"</td>";
-            st +="<td>"+stud[i].gender+"</td>";
-            st +="</tr>";
-        }    
+function display() {
+    let std = JSON.parse(localStorage.getItem("studentInfo"));
+    let sd = ''; // Declare and initialize outside the if block
+
+    if (std != null) {
+        sd += "<tr>";
+        sd += "<th>Name</th>";
+        sd += "<th>Age</th>";
+        sd += "<th>Salary</th>";
+        sd += "<th>Gender</th>";
+        sd += "</tr>";
+
+        for (let i = 0; i < std.studInfo.length; i++) {
+            sd += "<tr>";
+            sd += "<td>" + std.studInfo[i].name + "</td>";
+            sd += "<td>" + std.studInfo[i].age + "</td>";
+            sd += "<td>" + std.studInfo[i].salary + "</td>";
+            sd += "<td>" + std.studInfo[i].gender + "</td>";
+            sd += "</tr>";
+        }
     }
-    document.getElementById("studtable").innerHTML = st;
+    document.getElementById("studtable").innerHTML = sd;
 }
