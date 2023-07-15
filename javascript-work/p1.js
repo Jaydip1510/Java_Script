458b0b1cce7ef09b992974dc424560ca985df513
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2023-07-15 17:25:21 +0530

    changes

--- javascript-work/p1.js
+++ javascript-work/p1.js
@@ -1,7 +1,6 @@
 display();
 document.getElementById('btn1').addEventListener("click", () => {
     let name = document.userform.name.value;
-    console.log(name);
     let age = document.userform.age.value;
     let salary = document.userform.sal.value;
     let gen = document.userform.g1.value;
@@ -13,38 +12,38 @@ document.getElementById('btn1').addEventListener("click", () => {
         salary: salary,
         gender: gen
     };
-    
+
     let sData = JSON.parse(localStorage.getItem("empDetail"));
-    if (sData == null && sData.length >0) {
-        sData = [];
+    if (sData != null) {
         sData.push(xyz);
         localStorage.setItem("empDetail", JSON.stringify(sData));
     } else {
         jp = [xyz];
-        localStorage.setItem("empDetail", JSON.stringify(sData));
+        localStorage.setItem("empDetail", JSON.stringify(jp));
     }
     document.userform.reset();
     display();
 });
 function display() {
-  let dt = "<tr>";
-    dt += "<th><center>Name</center></th>";
-    dt += "<th><center>Age</center></th>";
-    dt += "<th><center>Salary</center></th>";
-    dt += "<th><center>Gender</center></th>";
-    dt += "<th><center>Action</center></th>";
-    dt += "</tr>";
-    let local = localStorage.getItem("empDetail");
-   
-    if (local != null) {
-       let abc = JSON.parse(local);    
+    let abc = JSON.parse(localStorage.getItem('empDetail'));
+    let dt = '';
+    if (abc != null) {
+        dt += "<tr>";
+        dt += "<th><center>Name</center></th>";
+        dt += "<th><center>Age</center></th>";
+        dt += "<th><center>Salary</center></th>";
+        dt += "<th><center>Gender</center></th>";
+        dt += "<th><center>Action</center></th>";
+        dt += "</tr>";
         for (let i = 0; i < abc.length; i++) {
+
+
             dt += "<tr>";
             dt += "<td><center>" + abc[i].name + "</center></td>";
             dt += "<td><center>" + abc[i].age + "</center></td>";
             dt += "<td><center>" + abc[i].salary + "</center></td>";
             dt += "<td><center>" + abc[i].gender + "</center></td>";
-            dt += "<td><center><input type='button' name='btndel' id='btndel' value='Delete' onclick = 'delData("+i+")'</center></td>";
+            dt += "<td><center><input type='button' name='btndel' id='btndel' value='Delete' onclick = 'delData(" + i + ")'</center></td>";
             dt += "</tr>";
         }
     }
