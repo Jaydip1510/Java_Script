53a39dabe6aa1cef9ac28f747edea53e22c89ae6
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-04-01 22:23:54 +0530

    changes

---
+++ class/simple.js
@@ -0,0 +1,21 @@
+function number(n) {
+     if(n<=100)
+     {
+        console.log(n); 
+        n++;
+        number(n);
+    }
+      
+} 
+var jp = 1;
+//number(jp);
+
+for(int i=1; i<=5; i++)
+{
+    for(int j=1; j<=i; j++)
+    {
+        //printf(*);
+    }
+}
+
+ return 0;
